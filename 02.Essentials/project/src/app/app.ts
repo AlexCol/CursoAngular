f0d82595430093aca56f1dd06b0f88763c339383
AlexCol <eu_axil@yahoo.com.br>
2026-05-05 13:11:32 -0300

    ...

--- 02.Essentials/project/src/app/app.ts
+++ 02.Essentials/project/src/app/app.ts
@@ -1,20 +1,24 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../components/header';
+import { TasksComponent } from '../components/tasks/tasks';
 import { DUMMY_USERS } from '../components/user/dummy-users';
 import { UserComponent } from '../components/user/user';
 import { appStyles } from './app.styles';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.html',
   //styleUrls: ['./app.css'],
 })
 export class App {
   protected readonly styles = appStyles;
   protected users = DUMMY_USERS;
+  protected selectedUser = this.users[0];
 
   onSelectUser(id: string) {
-    console.log('User selected!', id);
+    if (this.selectedUser.id === id) return;
+
+    this.selectedUser = this.users.find((user) => user.id === id) ?? this.selectedUser;
   }
 }
--- 02.Essentials/project/src/components/user/user.ts
+++ 02.Essentials/project/src/components/user/user.ts
@@ -1,6 +1,12 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { userStyles } from './user.styles';
 
+type User = {
+  id: string;
+  avatar: string;
+  name: string;
+};
+
 @Component({
   selector: 'app-user',
   imports: [],
@@ -10,9 +16,10 @@ export class UserComponent {
   protected readonly styles = userStyles;
 
   //! abordagem sem signals
-  @Input({ required: true }) id!: string;
-  @Input({ required: true }) avatar!: string;
-  @Input({ required: true }) name!: string;
+  // @Input({ required: true }) id!: string;
+  // @Input({ required: true }) avatar!: string;
+  // @Input({ required: true }) name!: string;
+  @Input({ required: true }) user!: User;
   @Output() select = new EventEmitter<string>();
 
   //! abordagem com signals
@@ -24,90 +31,13 @@ export class UserComponent {
   // select = output<string>();
 
   get imagePath() {
-    return `users/${this.avatar}`; //sem signals
+    return `users/${this.user.avatar}`; //sem signals
     //return computed(() => `users/${this.avatar()}`)(); //com signals
   }
 
   //! METHODS
   public onSelectUser() {
-    this.select.emit(this.id); //sem signals
+    this.select.emit(this.user.id); //sem signals
     //this.select.emit(this.id());
   }
 }
-
-//! com signals
-// import { Component, computed, signal } from '@angular/core';
-// import { DUMMY_USERS } from './dummy-users';
-// import { userStyles } from './user.styles';
-
-// @Component({
-//   selector: 'app-user',
-//   imports: [],
-//   templateUrl: './user.html',
-// })
-// export class UserComponent {
-//   protected readonly styles = userStyles;
-//   private currentUserIndex = 0;
-//   private selectedUser = signal(DUMMY_USERS[this.currentUserIndex]);
-//   public userData = computed(() => this.selectedUser());
-
-//   constructor() {
-//     this.selectedUser.set(DUMMY_USERS[this.getRandomIndex()]);
-//   }
-
-//   //! GETTERS
-//   get userAvatarPath() {
-//     return `users/${this.userData().avatar}`;
-//   }
-
-//   //! METHODS
-//   private getRandomIndex() {
-//     let newValue: number;
-//     do {
-//       newValue = Math.floor(Math.random() * DUMMY_USERS.length);
-//     } while (newValue === this.currentUserIndex);
-//     this.currentUserIndex = newValue;
-//     return newValue;
-//   }
-
-//   public onSelectUser() {
-//     this.selectedUser.set(DUMMY_USERS[this.getRandomIndex()]);
-//   }
-// }
-
-//! versão sem signal
-// import { Component } from '@angular/core';
-// import { userStyles } from './user.styles';
-// import { DUMMY_USERS } from './dummy-users';
-
-// @Component({
-//   selector: 'app-user',
-//   imports: [],
-//   templateUrl: './user.html',
-// })
-// export class UserComponent {
-//   protected readonly styles = userStyles;
-//   private selectedUser = DUMMY_USERS[0]
-
-//   constructor() {
-//     this.selectedUser = DUMMY_USERS[this.getRandomIndex()];
-//   }
-
-//   //! GETTERS
-//   get userAvatarPath() {
-//     return `users/${this.selectedUser.avatar}`;
-//   }
-
-//   get userData() {
-//     return this.selectedUser;
-//   }
-
-//   //! METHODS
-//   private getRandomIndex() {
-//     return Math.floor(Math.random() * DUMMY_USERS.length);
-//   }
-
-//   public onSelectUser() {
-//     this.selectedUser = DUMMY_USERS[this.getRandomIndex()];
-//   }
-// }
