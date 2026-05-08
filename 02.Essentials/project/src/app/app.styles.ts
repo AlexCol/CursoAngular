6f3584bbe7b9b1c2dc15fe6b48f444342599acdb
AlexCol <eu_axil@yahoo.com.br>
2026-05-08 15:18:14 -0300

    ..

--- 02.Essentials/project/src/app/app.styles.ts
+++ 02.Essentials/project/src/app/app.styles.ts
@@ -1,6 +1,7 @@
 export const appStyles = {
-  main: 'mx-auto grid w-[90%] max-w-[70rem] flex-1 gap-8 overflow-hidden pb-8 min-h-0 md:grid-cols-[1fr_3fr] md:pb-10',
+  main: 'mx-auto grid w-[90%] max-w-[70rem] flex gap-8 overflow-hidden pb-8 min-h-0 md:grid-cols-[1fr_3fr] md:pb-10',
   users:
     'm-0 flex list-none gap-2 overflow-auto p-0 md:min-h-0 md:flex-col max-h-[10vh] md:max-h-full',
+  tasksContainer: 'items-start justify-start min-h-0 w-full',
   fallback: 'm-0 text-center text-[1.15rem] font-bold md:text-left md:text-2xl',
 } as const;
