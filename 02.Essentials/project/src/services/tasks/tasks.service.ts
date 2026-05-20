ad390c815ea96e779156f84bc191a6ce6f341ccd
AlexCol <eu_axil@yahoo.com.br>
2026-05-20 12:59:18 -0300

    ..

--- 02.Essentials/project/src/services/tasks/tasks.service.ts
+++ 02.Essentials/project/src/services/tasks/tasks.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
-import { DUMMY_TASKS } from '../../components/tasks/dummy-tasks';
 import { NewTask } from '../../models/NewTask';
 import { Task } from '../../models/Task';
 
 @Injectable({ providedIn: 'root' })
 export class TasksService {
-  private tasks: Task[] = DUMMY_TASKS;
+  // private tasks: Task[] = DUMMY_TASKS;
+  private tasks: Task[];
+  constructor() {
+    this.tasks = JSON.parse(localStorage.getItem('tasks') || '[]');
+  }
 
   //! METHODS
   getTasksByUserId(userId: string): Task[] {
@@ -24,15 +27,21 @@ export class TasksService {
     };
 
     this.tasks.unshift(taskToAdd);
+    this.saveTasksToLocalStorage();
   }
 
   removeTask(taskId: string) {
     this.tasks = this.tasks.filter((task) => task.id !== taskId);
+    this.saveTasksToLocalStorage();
   }
 
   //! PRIVATE METHODS
   private getNextTaskId(): string {
     const nextId = Math.max(...this.tasks.map((task) => +task.id.replace('t', '')), 0) + 1;
     return `t${nextId}`;
+  }
+
+  private saveTasksToLocalStorage() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 }
