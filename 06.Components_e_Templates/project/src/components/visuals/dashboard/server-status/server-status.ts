8cffb2007ff79bf2d1f8bfc075f59e2d13785be2
AlexCol <eu_axil@yahoo.com.br>
2026-06-18 13:13:39 -0300

    .

--- 06.Components_e_Templates/project/src/components/visuals/dashboard/server-status/server-status.ts
+++ 06.Components_e_Templates/project/src/components/visuals/dashboard/server-status/server-status.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { ServerStatus } from '../../../../models/types/ServerStatus';
 import { serverStatusStyles } from './server-status.styles';
 
@@ -6,9 +6,42 @@ import { serverStatusStyles } from './server-status.styles';
   selector: 'app-server-status',
   templateUrl: './server-status.html',
   host: { class: serverStatusStyles.statusPanel },
+  imports: [],
 })
-export class ServerStatusComponent {
+export class ServerStatusComponent implements OnInit {
+  //!informação do implements OnInit abaixo
   protected readonly styles = serverStatusStyles;
+  currentStatusSignal = signal<ServerStatus>('unknown');
 
-  currentStatus: ServerStatus = 'online';
+  get currentStatus() {
+    return this.currentStatusSignal();
+  }
+
+  constructor() {
+    // const statuses: ServerStatus[] = ['online', 'offline', 'unknown'];
+    // const randomIndex = Math.floor(Math.random() * statuses.length);
+    // this.currentStatus = statuses[randomIndex];
+  }
+
+  ngOnInit() {
+    setInterval(() => {
+      //! pra de fato atualizar a UI, precisa ser usado Signal
+      const statuses: ServerStatus[] = ['online', 'offline', 'unknown'];
+      const randomIndex = Math.floor(Math.random() * statuses.length);
+      this.currentStatusSignal.set(statuses[randomIndex]);
+    }, 5000);
+  }
 }
+
+/***************************/
+/*
+O Angular não exige que a classe implemente a interface OnInit.
+Em tempo de execução ele apenas procura por um método chamado
+'ngOnInit' e o executa caso exista.
+
+Entretanto, implementar explicitamente a interface OnInit é
+recomendado porque o TypeScript passa a validar a existência
+e a assinatura do método. Isso ajuda a detectar erros de
+digitação como 'ngonInit', que compilariam normalmente mas
+nunca seriam chamados pelo Angular.
+*/
