2dc4f3c16fe380ea930561444fb76520f19a9065
AlexCol <eu_axil@yahoo.com.br>
2026-06-25 13:10:08 -0300

    ..

--- 07.Directives/project/src/components/app/app.ts
+++ 07.Directives/project/src/components/app/app.ts
@@ -1,13 +1,20 @@
-import { Component, signal } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, computed, signal } from '@angular/core';
 import { AuthComponent } from '../auth/auth';
+import { AuthService } from '../auth/auth.service';
 import { LearningResourcesComponent } from '../learning-resources/learning-resources';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
-  imports: [LearningResourcesComponent, AuthComponent],
+  imports: [LearningResourcesComponent, AuthComponent, NgIf],
 })
 export class App {
   protected readonly title = signal('project');
+  public isAdmin = computed(() => this.authService.activePermission() === 'admin');
+
+  //private authService = inject(AuthService);
+  // ou
+  constructor(private authService: AuthService) {}
 }
