2be550f435cd4b1e73fccc5296fdb8eb0432dc00
AlexCol <eu_axil@yahoo.com.br>
2026-07-13 12:59:41 -0300

    ..

--- 10.ChangeDetection/project/src/app/messages/messages-list/messages-list.component.ts
+++ 10.ChangeDetection/project/src/app/messages/messages-list/messages-list.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MessagesService } from '../messages.service';
 
 @Component({
@@ -7,23 +8,26 @@ import { MessagesService } from '../messages.service';
   templateUrl: './messages-list.component.html',
   styleUrl: './messages-list.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush, // observação do funcionamento em messages.component.ts
+  imports: [AsyncPipe],
 })
-export class MessagesListComponent implements OnInit {
+export class MessagesListComponent {
   private messageService = inject(MessagesService);
-  private cdRef = inject(ChangeDetectorRef); //! se não usar signal, precisamos injetar ChangeDetectorRef para forçar a atualização da view
-  private destroyRef = inject(DestroyRef);
+  messages$ = this.messageService.message$;
 
-  messages: string[] = [];
+  // private cdRef = inject(ChangeDetectorRef); //! se não usar signal, precisamos injetar ChangeDetectorRef para forçar a atualização da view
+  // private destroyRef = inject(DestroyRef);
 
-  ngOnInit() {
-    const subscription = this.messageService.message$.subscribe((messages: string[]) => {
-      this.messages = messages;
-      this.cdRef.markForCheck();
-    });
-    this.destroyRef.onDestroy(() => {
-      subscription.unsubscribe();
-    });
-  }
+  // messages: string[] = [];
+
+  // ngOnInit() {
+  //   const subscription = this.messageService.message$.subscribe((messages: string[]) => {
+  //     this.messages = messages;
+  //     this.cdRef.markForCheck();
+  //   });
+  //   this.destroyRef.onDestroy(() => {
+  //     subscription.unsubscribe();
+  //   });
+  // }
 
   get debugOutput() {
     console.log('[MessagesList] "debugOutput" binding re-evaluated.');
