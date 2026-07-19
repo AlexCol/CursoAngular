f03ea06729849df91ed3cfadf72baa8eb20bde39
AlexCol <eu_axil@yahoo.com.br>
2026-07-19 15:17:25 -0300

    ..

--- 12.Http/projeto/src/app/places/available-places/available-places.component.ts
+++ 12.Http/projeto/src/app/places/available-places/available-places.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
+import { map } from 'rxjs';
 import { Place } from '../place.model';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { PlacesComponent } from '../places.component';
@@ -14,16 +15,17 @@ import { PlacesComponent } from '../places.component';
 export class AvailablePlacesComponent implements OnInit {
   private httpCliente = inject(HttpClient);
   private destroyRef = inject(DestroyRef);
-
   places = signal<Place[] | undefined>(undefined);
 
   ngOnInit() {
     const subscription = this.httpCliente //api tem um delay de 3s
       .get<{ places: Place[] }>('http://localhost:3000/places', { observe: 'response' })
+      .pipe(map((response) => response.body?.places))
       .subscribe({
-        next: (response) => {
-          console.log(response); //¹
-          this.places.set(response.body?.places);
+        //recebendo places, pois foi ajustado no map para não vir a response inteira
+        next: (places) => {
+          console.log(places); //¹
+          this.places.set(places);
         },
         error: (error) => {
           console.log('Error fetching places:', error);
