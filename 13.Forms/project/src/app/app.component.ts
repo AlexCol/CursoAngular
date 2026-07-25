ca034d172ff96f29d1fb6fccb646aa351fb8295f
AlexCol <eu_axil@yahoo.com.br>
2026-07-25 17:17:12 -0300

    ...

--- 13.Forms/project/src/app/app.component.ts
+++ 13.Forms/project/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-
-import { LoginComponent } from './auth/login/login.component';
+import { LoginComponent } from './auth/login-reactive/login.component';
+//import { LoginComponent } from './auth/login-template/login.component';
 
 @Component({
   selector: 'app-root',
