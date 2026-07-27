56f84ee5e02bb3822843616d2aeed19d7aea73a1
AlexCol <eu_axil@yahoo.com.br>
2026-07-27 10:24:33 -0300

    ..

--- 14.Routing/project/src/app/users/user-tasks/user-tasks.component.ts
+++ 14.Routing/project/src/app/users/user-tasks/user-tasks.component.ts
@@ -1,4 +1,5 @@
-import { Component, computed, inject, input } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { UsersService } from '../users.service';
 
 @Component({
@@ -7,9 +8,27 @@ import { UsersService } from '../users.service';
   templateUrl: './user-tasks.component.html',
   styleUrl: './user-tasks.component.css',
 })
-export class UserTasksComponent {
-  private usersService = inject(UsersService);
+export class UserTasksComponent implements OnInit {
+  // forma 1, usnado computed e input
+  // private usersService = inject(UsersService);
+  // userId = input.required<string>(); //injetado automaticamente pela rota, pois o nome do input é o mesmo do parâmetro da rota (e temos na config withComponentInputBinding)
+  // userName = computed(() => this.usersService.users.find((user) => user.id === this.userId())?.name);
 
-  userId = input.required<string>(); //injetado automaticamente pela rota, pois o nome do input é o mesmo do parâmetro da rota (e temos na config withComponentInputBinding)
-  userName = computed(() => this.usersService.users.find((user) => user.id === this.userId())?.name);
+  //forma 2, usando o ActivatedRoute para pegar o parâmetro da rota
+  private usersService = inject(UsersService);
+  private activateRoute = inject(ActivatedRoute);
+  private destroyRef = inject(DestroyRef);
+  private _userName = '';
+  userName = () => this._userName; //feito assim só pra não precisar mudar o html, caso mude entre forma 1 e 2
+
+  ngOnInit(): void {
+    console.log(this.activateRoute);
+    const sub = this.activateRoute.params.subscribe((params) => {
+      const userId = params['userId'];
+      const user = this.usersService.users.find((user) => user.id === userId);
+      this._userName = user?.name ?? '';
+    });
+
+    this.destroyRef.onDestroy(() => sub.unsubscribe());
+  }
 }
