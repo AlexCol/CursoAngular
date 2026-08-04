d368c916a452a1432bd4b4e23f39ed9b70211d94
AlexCol <eu_axil@yahoo.com.br>
2026-08-04 13:22:48 -0300

    ..

--- 26.Components/project/src/app/app.ts
+++ 26.Components/project/src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { FirstComponent } from './components/first-component/first-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, FirstComponent],
+  imports: [FirstComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
--- 26.Components/project/src/app/components/first-component/first-component.ts
+++ 26.Components/project/src/app/components/first-component/first-component.ts
@@ -1,18 +1,37 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-first-component',
   imports: [FormsModule],
   templateUrl: './first-component.html',
   styleUrl: './first-component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FirstComponent {
   interpolation: string = 'This is an interpolation';
   dataBingingStyle: string = 'color: red; font-size: 20px;';
   twoWayBinding: string = 'This is a two-way binding';
 
+  get testeNaoComputado() {
+    const rand = Math.random();
+    console.log(`Random number generated: ${rand}`);
+    return rand;
+  }
+  testeComputado = computed(() => {
+    const rand = Math.random();
+    console.log(`Random number generated: ${rand}`);
+    return rand;
+  });
+
   onClick() {
     alert('You clicked the button!');
+  }
+
+  public submiedValue = signal<string>('');
+  onSubmit(form: NgForm) {
+    const value = form.value.name;
+    this.submiedValue.set(value);
+    alert(`Form submitted with value: ${value}`);
   }
 }
