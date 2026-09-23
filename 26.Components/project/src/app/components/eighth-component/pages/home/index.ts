1163eb326b209cd968d4cc5455e8eb172f834128
AlexCol <eu_axil@yahoo.com.br>
2026-09-23 12:46:01 -0300

    ..

--- 26.Components/project/src/app/components/eighth-component/pages/home/index.ts
+++ 26.Components/project/src/app/components/eighth-component/pages/home/index.ts
@@ -1,5 +1,6 @@
-import { Component, inject, OnDestroy, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnDestroy, signal } from '@angular/core';
 import { Router } from '@angular/router';
+import { finalize } from 'rxjs';
 import { Product } from '../../models/Product';
 import { AuthService } from '../../services/login.service';
 import { ProductsService } from '../../services/products.service';
@@ -8,11 +9,17 @@ import { ProductsService } from '../../services/products.service';
   selector: 'app-eighth-home',
   templateUrl: './home.html',
   styleUrls: ['./home.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeComponent implements OnDestroy {
   private _router = inject(Router);
   private _authService = inject(AuthService);
   private _productsService = inject(ProductsService);
+
+  private _isLoading = signal(false);
+  get isLoading() {
+    return this._isLoading();
+  }
 
   private _products = signal<Product[]>([]);
   get products() {
@@ -25,9 +32,13 @@ export class HomeComponent implements OnDestroy {
   }
 
   loadProducts() {
-    this._productsService.getAllProducts().subscribe((products) => {
-      this._products.set(products);
-    });
+    this._isLoading.set(true);
+    this._productsService
+      .getAllProducts()
+      .pipe(finalize(() => this._isLoading.set(false)))
+      .subscribe((products) => {
+        this._products.set(products);
+      });
   }
   ngOnDestroy() {
     this._products.set([]);
